00001680415a17be6b8738d49650ff06bf635200
Ed Flanagan <edsync256@gmail.com>
2014-07-08 22:16:00 +0000

    tune up bingo.py
    make shebang more 'dynamic'
    create width_type() to catch width-less-than-0 edge case
    tune up argeparse statements
    repace getFile() function with a with statement
    plays nicer with python3 (can't guarantee it works with 3 atm)

--- bingo.py
+++ bingo.py
@@ -1,5 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 import sys, random, textwrap, argparse
+
+# enforces that the width argument is above 0
+def width_type(w):
+	w = int(w)
+	if w <= 0:
+		raise argparse.ArgumentTypeError("width must be above 0")
+	return w
 
 # Reads inFile and converts each line in the file into a list, which is
 # then shuffled before being returned
@@ -13,16 +21,6 @@ def getList(inFile):
         bingoList.insert(12, "FREE SPACE")
         return bingoList[:25]
 
-# Returns a filehandler for the file at the path passed in as "name"
-def getFile(name):
-    inlist = ""
-    try:
-        inList = open(name, "r")
-        return inList
-    except IOError:
-        print "sorry, that file doesn't exist"
-        sys.exit()
-
 # Breaks each entry in the input list into chunks. Returns a list 
 # of list chunks
 def smallWords(bingoList, chunkSize):
@@ -34,30 +32,33 @@ def smallWords(bingoList, chunkSize):
     return bingoList
 
 def boardDump(bingoList, width):
-    print "=" * (6 + (width * 5))
-    print "|" + "B".center(width) + "|" + "I".center(width) + "|" + \
+    print("=" * (6 + (width * 5)))
+    print("|" + "B".center(width) + "|" + "I".center(width) + "|" + \
             "N".center(width) + "|" + "G".center(width) + "|" + \
-            "O".center(width) + "|"
-    print "=" * (6 + (width * 5))
+            "O".center(width) + "|")
+    print("=" * (6 + (width * 5)))
     for i in range(0,5):
         for j in range(0,3):
-            print "|" + bingoList[i*5][j].center(width) + "|" + \
+            print("|" + bingoList[i*5][j].center(width) + "|" + \
             bingoList[(i*5)+1][j].center(width) + "|" + \
             bingoList[(i*5)+2][j].center(width) + "|" + \
             bingoList[(i*5)+3][j].center(width) + "|" + \
-            bingoList[(i*5)+4][j].center(width) + "|"
-        print "=" * (6 + (width * 5))
+            bingoList[(i*5)+4][j].center(width) + "|")
+        print("=" * (6 + (width * 5)))
 
 # Main stuff
 parser = argparse.ArgumentParser(description='Generate a bingo board!')
-parser.add_argument('-f', '--file', nargs='?', const="bingo.list",\
-        dest='infile', type=str, required=True, \
-        help='File containing list of bingo words')
-parser.add_argument('-w', '--width', nargs=1, default="9",\
-        dest='width', type=int, help='The width of each cell on the board')
+parser.add_argument('-f', '--file', default="bingo.list", dest='infile', \
+        help='File containing the list of bingo words')
+parser.add_argument('-w', '--width', dest='width', default=9, 
+		type=width_type, \
+		help='The width of each cell on the board (default: 9)')
 args = parser.parse_args()
 
-inList = getFile(args.infile)
-width = int(args.width[0])
-
-boardDump(smallWords(getList(inList), width), width)
+try:
+	with open(args.infile, 'r') as inList:
+		width = args.width
+		boardDump(smallWords(getList(inList), width), width)
+		inList.close()
+except IOError:
+	print("Sorry, I can't open file: '{}'".format(args.infile))
